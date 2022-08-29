9178de0b316e8763dd8b55ce3e8c60943a8dc9d3
DanieleIudica <daniele.iudica@hotmail.it~>
2022-08-29 14:21:21 +0200

    push_es1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonComponent text="Visita il sito" />
+      <ImageComponent src="https://www.donnamoderna.com/content/uploads/2021/02/motivi-per-adottare-un-gatto.jpg" alt="gatto" />
     </div>
   );
 }
